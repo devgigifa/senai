d903f0abe4c4e642455d3d2311960b70ef9b0727
Giovanna Alves <giovanna.gigifa@gmail.com>
2024-07-03 16:44:23 -0300

    Update exercicios.js

--- ex-javascript/exercicios.js
+++ ex-javascript/exercicios.js
@@ -144,3 +144,19 @@ adicionarUsuario(usuarios, { nome: "Pedro", email: "pedro@example.com", senha: "
 adicionarUsuario(usuarios, { nome: "Ana", email: "ana@example.com", senha: "345678" });
 
 console.log(listarUsuarios(usuarios));
+
+// Matrizes
+
+let alunos = new Array(3)
+
+for(let i = 0; i < alunos.length; i++){
+    alunos[i] = new Array(2)
+    for(let j = 0; j < alunos[i].length; j++){
+        alunos[i][j] =prompt("Digite o nome do aluno:")
+    }
+}
+
+for(let elementos in alunos){
+   alunos[elementos] = prompt("Digite o nome do aluno:")
+}
+alert(alunos)
